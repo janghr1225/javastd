ab55388fc78a091100ce9e67184db8d0d9fb0b72
unknown <janghr1225@gmail.com>
2021-12-30 11:39:27 +0900

    Add std1.java

---
+++ std1.java
@@ -0,0 +1,26 @@
+package javastd1;
+
+import java.util.Scanner;
+
+public class std1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int x = scanner.nextInt();
+		int y = scanner.nextInt();
+		
+		
+		if(x>0 && y>0)
+	        System.out.println("1");
+	    if(x>0 && y<0)
+	        System.out.println("2");
+	    if(x<0 && y>0)
+	        System.out.println("3");
+	    if(x<0 && y<0)
+	        System.out.println("4");
+	}
+
+}
